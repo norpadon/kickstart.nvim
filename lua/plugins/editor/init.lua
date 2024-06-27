8d35ac575c4b9bcd786d0f33d005a11e59b5c709
Artur Chakhvadze <norpadon@yandex.ru>
2024-06-27 18:41:52 +0400

    Reimport plugins.

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -1,9 +1,9 @@
 return {
-  -- require 'plugins.editor.comment',
-  -- require 'plugins.editor.completion',
-  -- require 'plugins.editor.conform',
-  -- require 'plugins.editor.copilot',
-  -- require 'plugins.editor.lint',
-  -- require 'plugins.editor.tab',
-  -- require 'plugins.editor.telescope',
+  require 'plugins.editor.comment',
+  require 'plugins.editor.completion',
+  require 'plugins.editor.conform',
+  require 'plugins.editor.copilot',
+  require 'plugins.editor.lint',
+  require 'plugins.editor.tab',
+  require 'plugins.editor.telescope',
 }
